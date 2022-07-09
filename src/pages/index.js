63da1183e95b1de461e4fb502aaa3b5b3ef99e58
simonbeirouti <hello@simonbeirouti.com>
2022-07-09 23:24:30 +1000

    put components on index and SSP content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,31 @@
 import Header from "../components/Header";
 import MetaHead from "../components/MetaHead";
+import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <div>
+    <div className="bg-gray-100">
       <MetaHead
         title="Amazon Clone | Home"
         description="A project that replicates what Amazon looks like"
       />
       <Header />
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+        <ProductFeed products={products} />
+      </main>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      products,
+    },
+  };
 }
