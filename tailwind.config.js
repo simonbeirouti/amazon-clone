c77612b36523f961a68aca8bef1b58ec1b10a00b
simonbeirouti <hello@simonbeirouti.com>
2022-07-09 20:09:01 +1000

    setup postcss and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  content: ["./pages/**/*.js", "./components/**/*.js"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        amazon_blue: {
+          light: "#232F3E",
+          DEFAULT: "#131921",
+        },
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
+  plugins: [require("@tailwindcss/line-clamp")],
 };
