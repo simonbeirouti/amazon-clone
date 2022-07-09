824785e86c370d4eaebdabafa7b302ab3c7a1409
simonbeirouti <hello@simonbeirouti.com>
2022-07-09 14:33:41 +1000

    add tailwindcss to project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./pages/**/*.js", "./components/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+};
