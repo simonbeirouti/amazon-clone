3f0df12bc38b2af121a645ad757c866013f0a418
simonbeirouti <hello@simonbeirouti.com>
2022-07-10 17:37:42 +1000

    Link firebase credentials

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASLnldB9C5XvHOZ8HePOA_JJp50NavSOc",
+  authDomain: "besimon-5a682.firebaseapp.com",
+  projectId: "besimon-5a682",
+  storageBucket: "besimon-5a682.appspot.com",
+  messagingSenderId: "853719181970",
+  appId: "1:853719181970:web:2fcc068fbd1abb2799544b",
+};
