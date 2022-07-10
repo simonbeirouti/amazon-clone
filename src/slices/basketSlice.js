39df89f749ae54a130c8db983d8361fe5def1da8
simonbeirouti <hello@simonbeirouti.com>
2022-07-10 17:38:12 +1000

    Create add, remove and total states to basket

--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -8,13 +8,29 @@ export const basketSlice = createSlice({
   name: "basket",
   initialState,
   reducers: {
-    addToBasket: (state, action) => {},
-    removeFromBasket: (state, action) => {},
+    addToBasket: (state, action) => {
+      state.items = [...state.items, action.payload];
+    },
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex(
+        (basketItem) => basketItem.id === action.payload.id
+      );
+      let newBasket = [...state.items];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(`Cant remove the product ${action.payload.id}`);
+      }
+      state.items = newBasket;
+    },
   },
 });
 
 export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 export const selectItems = (state) => state.basket.items;
+export const selectTotal = (state) =>
+  state.basket.items.reduce((total, item) => total + item.price, 0);
 
 export default basketSlice.reducer;
